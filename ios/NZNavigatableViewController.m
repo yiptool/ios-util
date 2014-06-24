c759f5fcbd5fed9424f3b2b041c6badb4fb3e5be
Nikolay Zapolnov <zapolnov@gmail.com>
2014-06-24 19:35:08 +0400

    Added the NZNavigatableViewController class.

---
+++ ios/NZNavigatableViewController.m
@@ -0,0 +1,47 @@
+/* vim: set ai noet ts=4 sw=4 tw=115: */
+//
+// Copyright (c) 2014 Nikolay Zapolnov (zapolnov@gmail.com).
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#import "NZNavigatableViewController.h"
+
+@implementation NZNavigatableViewController
+{
+	BOOL navigationBarWasHidden;
+}
+
+@synthesize navigationBarHidden;
+
+-(void)viewWillAppear:(BOOL)animated
+{
+	navigationBarWasHidden = self.navigationController.navigationBarHidden;
+	if (navigationBarWasHidden != navigationBarHidden)
+		[self.navigationController setNavigationBarHidden:navigationBarHidden animated:animated];
+	[super viewWillAppear:animated];
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+	if (self.navigationController.navigationBarHidden != navigationBarWasHidden)
+		[self.navigationController setNavigationBarHidden:navigationBarWasHidden animated:animated];
+	[super viewWillDisappear:animated];
+}
+
+@end
